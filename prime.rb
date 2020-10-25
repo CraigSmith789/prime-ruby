9077b882dcaebe7feabee15bd3352460e659abad
CraigSmith789 <craig789@gmail.com>
2020-10-25 17:03:09 -0400

    commit tree 2767071ed779f593ae29bd712eabd5e4746aa8b8

--- prime.rb
+++ prime.rb
@@ -1,1 +1,36 @@
-# Add  code here!
+def prime?(int) 
+    if int <= 1 
+        return false 
+    end
+    
+    if (2...int).any?{|i| int %i == 0}
+        false 
+    else 
+        return true 
+    end
+
+end
+
+
+
+
+
+
+
+
+
+
+# def prime?(int)
+#     if int <= 1
+#         return false 
+#     end
+#  i=2
+#  while i < int
+#     if int %1 == 0
+#         false
+#     else 
+#         i +=1
+#     end
+# end
+#     true
+# end
